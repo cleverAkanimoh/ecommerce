d3c0331172bc205c547ae529d22288c4f5d27366
cleverakanimoh <cleverakanimoh02@gmail.com>
2024-04-16 08:58:58 +0100

    changed 12 files

---
+++ reducer.js
@@ -0,0 +1,27 @@
+import * as actions from "./action-types";
+
+let lastId = 0;
+
+export default function reducer(state = [], action) {
+  switch (action.type) {
+    case actions.Bug_Added:
+      return [
+        ...state,
+        {
+          id: ++lastId,
+          description: action.payload.description,
+          resolved: false,
+        },
+      ];
+    case actions.Bug_Removed:
+      return state.filter((bug) => bug.id !== action.payload.id);
+
+    case actions.Bug_Resolved:
+      return state.map((bug) =>
+        bug.id !== action.payload.id ? bug : { ...bug, resolved: true }
+      );
+
+    default:
+      return state;
+  }
+}
